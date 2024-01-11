f67e615bca104035398cba139e982cca22da9cf2
luciano lawson <lucianoglawson@yahoo.com>
2024-01-10 19:16:12 -0600

    finished code code basics are done but the spacing of the words are close

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import { useState } from 'react'
 import './App.css'
-
+import { Routes, Route, Link } from "react-router-dom"
+import Red from './components/Red'
+import Blue from './components/Blue'
+import Home from './components/Home'
 function App() {
-
-
   return (
-    <>
-      <div>
-
+<div id="container">
+      <div id="navbar">
+        <Link>
+          <Link to="/blue">Blue</Link>
+          <Link to="/red">Red</Link>
+          <Link to="/">Home</Link>
+        </Link>
       </div>
-    </>
+      <div id="main-section">
+          <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/red" element={<Red />} />
+      <Route path="/blue" element={<Blue />} />
+    </Routes>
+        </div>
+    </div>
   )
 }
 
