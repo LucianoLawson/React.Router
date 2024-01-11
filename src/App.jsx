65b5491577954436271282a0ab436a4251dd6c36
luciano lawson <lucianoglawson@yahoo.com>
2024-01-10 19:20:07 -0600

    spacing slightly fixed the spacing issuses

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,20 @@ function App() {
 <div id="container">
       <div id="navbar">
         <Link>
-          <Link to="/blue">Blue</Link>
-          <Link to="/red">Red</Link>
-          <Link to="/">Home</Link>
+          <Link to="/blue"> Blue </Link>
+          <Link to="/red"> Red </Link>
+          <Link to="/"> Home </Link>
         </Link>
       </div>
       <div id="main-section">
           <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/red" element={<Red />} />
-      <Route path="/blue" element={<Blue />} />
+      <Route path="/" element={< Home />} />
+      <Route path="/red" element={< Red />} />
+      <Route path="/blue" element={< Blue />} />
     </Routes>
         </div>
     </div>
   )
 }
 
-export default App
+export default App;
